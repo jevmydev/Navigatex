5017d8e0d319d54de4ba283ac1840ba479f350b3
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-20 20:47:24 -0300

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
+import { Anchor } from "../elements/Anchor";
+
 export function Footer() {
-    return <footer className="text-center mt-32">Creado moviendo las manitas y con mucho amor</footer>;
+    return (
+        <footer>
+            <div className="max-w-screen-sm mt-32 mx-auto flex flex-col items-center gap-4">
+                <p>Desarrollado moviendo las manitas y con mucho amor</p>
+                <div className="flex items-center gap-4">
+                    <Anchor href="https://jeremydiaz.netlify.app/" target="_blank">
+                        Creado por jevmydev
+                    </Anchor>
+                    <Anchor href="https://github.com/jevmydev/navigatex" target="_blank">
+                        Código abierto
+                    </Anchor>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
