96e0bb6f1fe0c8d8127b6f5b16bbd506580b660a
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-20 18:28:40 -0300

    Update responsive, design input and add blur

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,13 +1,13 @@
 import { Button } from "../elements/Button";
+import { Blur } from "../elements/Blur";
 import { SendIcon } from "../elements/Icons";
 
 export function SearcherForm({ search, handleChange, handleSubmit }) {
     return (
         <form className="relative" autoComplete="off" onSubmit={handleSubmit}>
-            <div className="input-degraded-pulse absolute -z-10 w-full h-full -left-px -top-px border border-transparent rounded-full box-content bg-indigo-600"></div>
             <div>
                 <input
-                    className="bg-slate-800 border border-transparent outline-none rounded-full py-2 px-4 w-full placeholder:text-slate-400"
+                    className=" bg-slate-800 border border-slate-600 transition-colors outline-none rounded-full py-2 px-4 w-full placeholder:text-slate-400 focus:border-indigo-600"
                     name="search"
                     type="search"
                     autoFocus
@@ -30,10 +30,11 @@ export function Searcher({ children }) {
         <section>
             <div className="max-w-screen-sm mx-auto font-light flex flex-col justify-end items-center gap-8 h-96">
                 <div className="flex flex-col font-bold">
-                    <h1 className="tracking-tighter text-indigo-600 text-6xl sm:text-8xl">NavigateX</h1>
+                    <h1 className="tracking-tighter text-indigo-600 text-5xl mb:text-6xl desk:text-8xl">NavigateX</h1>
                     <h2 className="tracking-tighter">Navega la web con IA</h2>
                 </div>
                 <div className="w-full">{children}</div>
+                <Blur />
             </div>
         </section>
     );
