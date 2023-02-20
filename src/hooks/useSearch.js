15761b9acb012b2f1a468472508e973c3cecf3ed
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-20 18:25:10 -0300

    Add submit search and remove comments

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -3,13 +3,14 @@ import { useState, useRef } from "react";
 export function useSearch() {
     const [search, setSearch] = useState("");
     const isFirstTimeInput = useRef(true);
+    const searchSubmit = useRef("");
 
     const updateSearch = ({ search }) => {
         if ((search === "" && isFirstTimeInput.current) || search.startsWith(" ")) return;
 
         isFirstTimeInput.current = false;
-        setSearch(search); /* TRIM */
+        setSearch(search);
     };
 
-    return { search, updateSearch };
+    return { search, searchSubmit, updateSearch };
 }
