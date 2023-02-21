220367e5ec35477c700110ac97f1dd234c111355
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-21 15:53:34 -0300

    Update adjust search in responsive

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -3,7 +3,7 @@ import { Anchor } from "../elements/Anchor";
 export function ResponseArticle({ title, question, response, children }) {
     return (
         <div className="h-full flex flex-col gap-2 bg-slate-800 p-4 rounded">
-            <h2 className="flex items-center gap-1">
+            <h2 className="flex flex-col gap-1">
                 {title}
                 <span className="tracking-tighter text-indigo-600 text-xl font-bold">{question}</span>
             </h2>
