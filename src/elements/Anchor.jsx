ef78402e81886e3a5a8cee2a77c497f43e3b089c
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-21 15:52:20 -0300

    Update Anchor and Button component

--- src/elements/Anchor.jsx
+++ src/elements/Anchor.jsx
@@ -1,7 +1,7 @@
 export function Anchor({ href, target, children }) {
     return (
         <a
-            className="text-white text-center font-bold w-full px-4 py-2 bg-slate-700 border border-transparent outline-none rounded-full whitespace-nowrap transition-colors hover:bg-slate-600 active:bg-transparent active:border-slate-600 focus-visible:border-slate-600"
+            className="flex items-center gap-2 text-white text-center font-bold w-full px-4 py-2 bg-slate-700 border border-transparent outline-none rounded whitespace-nowrap transition-colors hover:bg-slate-600 active:bg-transparent active:border-slate-600 focus-visible:border-slate-600"
             href={href}
             target={target}
         >
--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -1,6 +1,10 @@
-export function Button({ children }) {
+export function Button({ children, type = "button", onClick, className }) {
     return (
-        <button className="bg-slate-800 p-2 border border-transparent outline-none rounded-full transition-colors hover:bg-slate-700 active:bg-transparent active:border-slate-600 focus-visible:border-slate-600">
+        <button
+            type={type}
+            className={`flex items-center whitespace-nowrap gap-2 bg-slate-800 p-2 border border-transparent outline-none rounded transition-colors hover:bg-slate-700 active:bg-transparent active:border-slate-600 focus-visible:border-slate-600 ${className}`}
+            onClick={onClick}
+        >
             {children}
         </button>
     );
