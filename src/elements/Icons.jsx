3c3b9943fcb50ec4fbc79a57e4d64dd4a9b26328
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-21 15:52:35 -0300

    Add new icons

--- src/elements/Icons.jsx
+++ src/elements/Icons.jsx
@@ -1,6 +1,6 @@
-export function FavIcon() {
+export function FavIcon({ width = "w-8", height = "h-8" }) {
     return (
-        <svg className="w-8 h-8" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <circle cx="12" cy="12" r="9" />
             <line x1="3.6" y1="9" x2="20.4" y2="9" />
@@ -11,9 +11,9 @@ export function FavIcon() {
     );
 }
 
-export function SettingIcon() {
+export function SettingIcon({ width = "w-8", height = "h-8" }) {
     return (
-        <svg className="w-8 h-8" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
             <path stroke="none" fill="none" d="M0 0h24v24H0z" />
             <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
             <circle cx="12" cy="12" r="3" />
@@ -21,9 +21,9 @@ export function SettingIcon() {
     );
 }
 
-export function UserIcon() {
+export function UserIcon({ width = "w-8", height = "h-8" }) {
     return (
-        <svg className="w-8 h-8" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <circle cx="12" cy="12" r="9" />
             <circle cx="12" cy="10" r="3" />
@@ -32,12 +32,47 @@ export function UserIcon() {
     );
 }
 
-export function SendIcon() {
+export function SendIcon({ width = "w-4", height = "h-4" }) {
     return (
-        <svg className="w-4 h-4" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <line x1="10" y1="14" x2="21" y2="3" />
             <path d="M21 3l-6.5 18a0.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a0.55 .55 0 0 1 0 -1l18 -6.5" />
+        </svg>
+    );
+}
+
+export function MicIcon({ width = "w-6", height = "h-6" }) {
+    return (
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <rect x="9" y="2" width="6" height="11" rx="3" />
+            <path d="M5 10a7 7 0 0 0 14 0" />
+            <line x1="8" y1="21" x2="16" y2="21" />
+            <line x1="12" y1="17" x2="12" y2="21" />
+        </svg>
+    );
+}
+
+export function MicMuteIcon({ width = "w-6", height = "h-6" }) {
+    return (
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <line x1="3" y1="3" x2="21" y2="21" />
+            <path d="M9 5a3 3 0 0 1 6 0v5a3 3 0 0 1 -.13 .874m-2 2a3 3 0 0 1 -3.87 -2.872v-1" />
+            <path d="M5 10a7 7 0 0 0 10.846 5.85m2.002 -2a6.967 6.967 0 0 0 1.152 -3.85" />
+            <line x1="8" y1="21" x2="16" y2="21" />
+            <line x1="12" y1="17" x2="12" y2="21" />
+        </svg>
+    );
+}
+
+export function CloseIcon({ width = "w-6", height = "h-6" }) {
+    return (
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
         </svg>
     );
 }
