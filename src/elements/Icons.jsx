426c7386448d8cda1cebc44563a5370548a55f47
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-22 20:48:00 -0300

    Update write icon

--- src/elements/Icons.jsx
+++ src/elements/Icons.jsx
@@ -32,6 +32,16 @@ export function UserIcon({ width = "w-8", height = "h-8" }) {
     );
 }
 
+export function WriteIcon({ width = "w-8", height = "h-8" }) {
+    return (
+        <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
+            <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
+        </svg>
+    );
+}
+
 export function SendIcon({ width = "w-4", height = "h-4" }) {
     return (
         <svg className={`${width} ${height}`} viewBox="0 0 24 24" stroke="currentColor" fill="none">
